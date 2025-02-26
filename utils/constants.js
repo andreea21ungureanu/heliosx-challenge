662c1b5d8ba2e2923d8fa114ee10672fd429823d
andreea21ungureanu <andreea.ungureanu221@gmail.com>
2025-02-26 17:25:56 +0000

    create basic functionality + constants file

---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+export const PEAR_ALLERGY_QUESTIONS = [
+  "Have you ever experienced itching, swelling, or a rash after eating pears or similar fruits?",
+  "Do you have a known allergy to pome fruits like apples, pears, or quinces?",
+  "Have you ever had difficulty breathing, nausea, or dizziness after consuming pears?",
+  "Do you have a history of oral allergy syndrome (tingling or swelling in the mouth) after eating raw fruits?",
+  "Have you ever been diagnosed with a latex or birch pollen allergy, which can be linked to pear allergies?",
+];
+
+export const QUESTIONS_ANSWERS_OPTIONS = { Yes: "Yes", No: "No" };
